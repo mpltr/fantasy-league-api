d7d8b9670deed24961b2d14949af5adeb3683c94
Matt Poulter <matt.poulter@apsleygroup.co.uk>
2020-09-01 22:26:44 +0100

    database config fixed

--- config/database.php
+++ config/database.php
@@ -1,21 +1,24 @@
 <?php
 
-$DATABASE_URL=parse_url("postgres://wcrbaarmpseixj:1bff2c9412ee9476bf5bdd3a6f234b06001a5ad2d4f494e738009f3315dc9492@ec2-54-236-169-55.compute-1.amazonaws.com:5432/db0q936stg63at");
+$url = parse_url(getenv("DATABASE_URL"));
+
+$host = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$database = substr($url["path"], 1);
+
 return [
     'default' => 'pgsql',
     'connections' => [
-        'mysql' => [
+        'pgsql' => [
             'driver' => 'pgsql',
-            'host' => $DATABASE_URL["host"], 
-            'port' => $DATABASE_URL["port"],
-            'database' => ltrim($DATABASE_URL["path"], "/"),
-            'username' => $DATABASE_URL["user"],
-            'password' => $DATABASE_URL["pass"],
+            'host' => $host, 
+            'database' => $database,
+            'username' => $username,
+            'password' => $password,
             'charset'   => 'utf8',
             'prefix'  => '',
-            'prefix_indexes' => true, 
             'schema' => 'public',
-            'sslmode' => 'prefer'
         ]
     ]
 ];
